b02409b150fa67f871fce56a28801c3f3289a1cc
adamw <adam@warski.org>
2020-08-31 10:59:29 +0200

    Change Pipe type for ZIO

--- zio/src/main/scala/sttp/capabilities/zio/ZioStreams.scala
+++ zio/src/main/scala/sttp/capabilities/zio/ZioStreams.scala
@@ -2,16 +2,16 @@ package sttp.capabilities.zio
 
 import sttp.capabilities.Streams
 import zio.blocking.Blocking
-import zio.stream.{Stream, Transducer, ZStream, ZTransducer}
+import zio.stream.{Stream, ZStream}
 
 trait ZioStreams extends Streams[ZioStreams] {
   override type BinaryStream = Stream[Throwable, Byte]
-  override type Pipe[A, B] = Transducer[Throwable, A, B]
+  override type Pipe[A, B] = Stream[Throwable, A] => Stream[Throwable, B]
 }
 object ZioStreams extends ZioStreams
 
 trait BlockingZioStreams extends Streams[BlockingZioStreams] {
   override type BinaryStream = ZStream[Blocking, Throwable, Byte]
-  override type Pipe[A, B] = ZTransducer[Blocking, Throwable, A, B]
+  override type Pipe[A, B] = ZStream[Blocking, Throwable, A] => ZStream[Blocking, Throwable, B]
 }
 object BlockingZioStreams extends BlockingZioStreams
