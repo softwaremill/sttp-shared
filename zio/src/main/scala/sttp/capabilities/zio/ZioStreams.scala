1c2e1a59d64bef2126d59ce31b330f4e23e11b01
adamw <adam@warski.org>
2020-09-29 15:48:05 +0200

    Remove BlockingZioStreams

--- zio/src/main/scala/sttp/capabilities/zio/ZioStreams.scala
+++ zio/src/main/scala/sttp/capabilities/zio/ZioStreams.scala
@@ -1,17 +1,10 @@
 package sttp.capabilities.zio
 
 import sttp.capabilities.Streams
-import zio.blocking.Blocking
-import zio.stream.{Stream, ZStream}
+import zio.stream.Stream
 
 trait ZioStreams extends Streams[ZioStreams] {
   override type BinaryStream = Stream[Throwable, Byte]
   override type Pipe[A, B] = Stream[Throwable, A] => Stream[Throwable, B]
 }
 object ZioStreams extends ZioStreams
-
-trait BlockingZioStreams extends Streams[BlockingZioStreams] {
-  override type BinaryStream = ZStream[Blocking, Throwable, Byte]
-  override type Pipe[A, B] = ZStream[Blocking, Throwable, A] => ZStream[Blocking, Throwable, B]
-}
-object BlockingZioStreams extends BlockingZioStreams
