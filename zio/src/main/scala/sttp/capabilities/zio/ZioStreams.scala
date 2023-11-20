28f6901d810eec3251233c0c1b29fa2fc899d1fe
kciesielski <kciesielski@users.noreply.github.com>
2023-11-20 16:21:56 +0100

    Move methods to object to avoid Mima issues

--- zio/src/main/scala/sttp/capabilities/zio/ZioStreams.scala
+++ zio/src/main/scala/sttp/capabilities/zio/ZioStreams.scala
@@ -1,7 +1,7 @@
 package sttp.capabilities.zio
 
 import sttp.capabilities.Streams
-import zio.stream.{ Stream, ZStream }
+import zio.stream.{Stream, ZStream}
 import zio.stream.ZChannel
 import zio.Trace
 import zio.Chunk
@@ -11,18 +11,22 @@ import sttp.capabilities.StreamMaxLengthExceeded
 trait ZioStreams extends Streams[ZioStreams] {
   override type BinaryStream = Stream[Throwable, Byte]
   override type Pipe[A, B] = Stream[Throwable, A] => Stream[Throwable, B]
+}
+
+object ZioStreams extends ZioStreams {
 
   def limitBytes(stream: Stream[Throwable, Byte], maxBytes: Long): Stream[Throwable, Byte] =
-    scanChunksAccum(stream, initState = 0L) {
-       case (accumulatedBytes, chunk) =>
-       val byteCount = accumulatedBytes + chunk.size
-       if (byteCount > maxBytes)
-         throw new StreamMaxLengthExceeded(maxBytes)
-       else
-         byteCount
+    scanChunksAccum(stream, initState = 0L) { case (accumulatedBytes, chunk) =>
+      val byteCount = accumulatedBytes + chunk.size
+      if (byteCount > maxBytes)
+        throw new StreamMaxLengthExceeded(maxBytes)
+      else
+        byteCount
     }
 
-  def scanChunksAccum[S, R, A](inputStream: ZStream[R, Throwable, A], initState: => S)(f: (S, Chunk[A]) => S)(implicit trace: Trace): ZStream[R, Throwable, A] =
+  def scanChunksAccum[S, R, A](inputStream: ZStream[R, Throwable, A], initState: => S)(
+      f: (S, Chunk[A]) => S
+  )(implicit trace: Trace): ZStream[R, Throwable, A] =
     ZStream.succeed(initState).flatMap { state =>
       def accumulator(currS: S): ZChannel[Any, Throwable, Chunk[A], Any, Throwable, Chunk[A], Unit] =
         ZChannel.readWith(
@@ -40,6 +44,4 @@ trait ZioStreams extends Streams[ZioStreams] {
 
       ZStream.fromChannel(inputStream.channel >>> accumulator(state))
     }
-
 }
-object ZioStreams extends ZioStreams
