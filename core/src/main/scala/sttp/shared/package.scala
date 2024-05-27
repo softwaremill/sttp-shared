abbe8f325e545940633222e5b79fabf103387ca3
adamw <adam@warski.org>
2024-05-27 19:04:36 +0200

    Docs

--- core/src/main/scala/sttp/shared/package.scala
+++ core/src/main/scala/sttp/shared/package.scala
@@ -1,5 +1,9 @@
 package sttp
 
 package object shared {
+
+  /** The `Identity` type constructor can be used where an "effect" or wrapper (usually called `F[_]`) is expected. It
+    * represents direct-style / synchronous programming, and allows passing in code written in this style.
+    */
   type Identity[X] = X
 }
